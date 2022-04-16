0c0499e576efd796322e94c5204df45a0abe7393
Zep <52792159+CY031102@users.noreply.github.com>
2022-04-16 19:28:26 +0800

    SELinuxOFF.sh_v1.1.0

--- SELinuxOFF.sh
+++ SELinuxOFF.sh
@@ -1,35 +1,104 @@
 menu(){
-    echo "This is a Shell Script to Disable SELinux on CentOS 7"
-    echo "這是關閉CentOS7 SELinux的一鍵脚本"
-    echo "1. Disable SELinux_關閉SELinux"
-    echo "0. Exit_退出脚本"
+    echo "This is a Shell Script to Disable SELinux & firewalld on CentOS 7"
+    echo "這是關閉CentOS7 SELinux & firewalld 的一鍵脚本"
+    echo "1. Disable SELinux & firewalld"
+	echo "   關閉SELinux & Firewalld"
+	echo "2. Disable SELinux Only"
+	echo "   僅關閉SElinux"
+	echo "3. Disable firewalld Only"
+	echo "   僅關閉 firewalld"
+    echo "0. Exit"
+	echo "   退出脚本"
     read -n2 -p "Please Enter an Option_請輸入選項：" menuChoose
     case ${menuChoose} in
         1) shutselfire ;;
+		2) shutsel ;;
+		3) shutfire ;;
         0) exit 0
     esac
 }
 
 shutselfire(){
+	check_two=`systemctl status firewalld | grep "dead"`
+	if [[ $check_two ]]
+	then
+		echo "firewalld is inactive"
+		echo "firewalld 已關閉或未啓動"
+	else
+		echo "Detected firewalld is running/dead/not installed, will initialize firewalld shutdown"
+		echo "檢測firewalld可能在運行中/不存在，開始執行關閉"
+		echo "System will initialize the process in 3 seconds, use Ctrl+C to cancel"
+		echo "系統將在3秒后開始關閉firewalld，使用Ctrl+C取消"
+		#sleep-for-3-seconds_等待3秒
+		sleep 3s
+		systemctl stop firewalld.service
+		systemctl disable firewalld.service
+	fi
+	
 	check=`sestatus | grep "dis"`
 	if [[ $check ]]
 	then
-		echo "SELinux Disabled_SELinux未启动"
+		echo "SELinux Disabled"
+		echo "SELinux 已禁用"
 	else
 		echo "Detected SELinux enabled, starting disable process"
-		echo "檢測SELunix開啓，開始執行關閉"
+		echo "檢測SELunix已啓用，開始執行關閉"
 		echo "Systm will initialize the process in 3 seconds, use Ctrl+C to cancel"
 		echo "系統將在3秒后開始關閉SELunix，使用Ctrl+C取消"
 		#sleep-for-3-seconds_等待3秒
 		sleep 3s
 		sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 		sed -i 's/SELINUXTYPE=targeted/#SELINUXTYPE=targeted/g' /etc/selinux/config
-		#reboot_重启
-		echo "The system will reboot in 5 seconds, press Ctrl+C to cancel"
-		echo "系統會在5秒後重新啓動，使用Ctrl+C取消"
+		#reboot_重新啓動
+		echo "A reboot is requiered to Completely disable SELinux, the process will initiate in 5 seconds, use Ctrl+C to cancel"
+		echo "由於完整關閉SELinux需要重新啓動，系統會在5秒後重啓，使用Ctrl+C取消"
 		sleep 5s
 		reboot
 	fi
+	
+}	
+
+shutsel(){
+	check=`sestatus | grep "dis"`
+	if [[ $check ]]
+	then
+		echo "SELinux Disabled"
+		echo "SELinux 已禁用"
+	else
+		echo "Detected SELinux enabled, starting disable process"
+		echo "檢測SELunix已啓用，開始執行關閉"
+		echo "Systm will initialize the process in 3 seconds, use Ctrl+C to cancel"
+		echo "系統將在3秒后開始關閉SELunix，使用Ctrl+C取消"
+		#sleep-for-3-seconds_等待3秒
+		sleep 3s
+		sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+		sed -i 's/SELINUXTYPE=targeted/#SELINUXTYPE=targeted/g' /etc/selinux/config
+		#reboot_重新啓動
+		echo "A reboot is requiered to Completely disable SELinux, the process will initiate in 5 seconds, use Ctrl+C to cancel"
+		echo "由於完整關閉SELinux需要重新啓動，系統會在5秒後重啓，使用Ctrl+C取消"
+		sleep 5s
+		reboot
+	fi
+	
+}	
+
+shutfire(){
+	check_two=`systemctl status firewalld | grep "dead"`
+	if [[ $check_two ]]
+	then
+		echo "firewalld is inactive"
+		echo "firewalld 已關閉或未啓動"
+	else
+		echo "Detected firewalld is running/dead/not installed, will initialize firewalld shutdown"
+		echo "檢測firewalld可能在運行中/不存在，開始執行關閉"
+		echo "System will initialize the process in 3 seconds, use Ctrl+C to cancel"
+		echo "系統將在3秒后開始關閉firewalld，使用Ctrl+C取消"
+		#sleep-for-3-seconds_等待3秒
+		sleep 3s
+		systemctl stop firewalld.service
+		systemctl disable firewalld.service
+	fi
+
 }
 
 menu
